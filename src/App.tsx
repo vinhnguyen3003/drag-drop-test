776ffce3914b97adb393f8d0df79d9b0685a6a57
kutekeyzbo <kutekeyzbo@gmail.com>
2024-01-06 16:38:26 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,13 @@ import Admin from './pages/admin';
 import Consumer from './pages/consumer';
 
 function App() {
-
   return (
     <>
        <BrowserRouter>
         <Routes>
-			<Route path="/" element={<Navigate to="/admin" />}/>
-			<Route path="/admin" element={<Admin />} />
-			<Route path="/consumer" element={<Consumer />} />
+          <Route path="/*" element={<Navigate to="/admin" />}/>
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/consumer" element={<Consumer />} />
         </Routes>
       </BrowserRouter>
     </>
